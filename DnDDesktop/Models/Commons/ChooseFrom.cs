4d229aa54ef72203cb28b8efc6230d82df803a0d
Jironiah <seandertal@gmail.com>
2024-04-19 16:23:57 +0200

    Modelos comunes añadidos

---
+++ DnDDesktop/Models/Commons/ChooseFrom.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DnDDesktop.Models.Commons
+{
+    [BsonNoId]
+    public class ChooseFrom
+    {
+        [BsonElement("choose")]
+        public int? Choose { get; set; }
+
+        [BsonElement("from")]
+        public string[]? From { get; set; }
+    }
+}
---
+++ DnDDesktop/Models/Commons/UnitQuantity.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDDesktop.Models.Commons
+{
+    [BsonNoId]
+    public class UnitQuantity
+    {
+        [BsonElement("quantity")]
+        public double? Quantity { get; set; }
+
+        [BsonElement("unit")]
+        public string String { get; set; } = String.Empty;
+    }
+}

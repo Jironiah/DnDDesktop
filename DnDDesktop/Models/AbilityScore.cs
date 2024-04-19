ff52a06af8107b42840f9c020b23b8dec41776f5
Jironiah <seandertal@gmail.com>
2024-04-19 16:44:53 +0200

    Models terminado

--- DnDDesktop/Models/AbilityScore.cs
+++ DnDDesktop/Models/AbilityScore.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DnDDesktop.Models.Commons;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace DnDDesktop.Models
 {
     public class AbilityScore
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("index")]
+        public string Index { get; set; } = string.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonElement("full_name")]
+        public string FullName { get; set; } = string.Empty;
+
+        [BsonElement("desc")]
+        public string[]? Description { get; set; }
+
+        [BsonElement("skills")]
+        public From[]? Skills { get; set;}
     }
 }
---
+++ DnDDesktop/Models/IDndStoreDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace DnDDesktop.Models
+{
+    public interface IDndStoreDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName {  get; set; }
+        string DndDamageTypeCollectionName { get; set; }
+        string DndAlignmentCollectionName { get; set; }
+        string DndWeaponPropertyCollectionName { get; set; }
+        string DndConditionsCollectionName { get; set; }
+        string DndLanguageCollectionName { get; set; }
+        string DndSkillCollectionName { get; set; }
+        string DndAbilityScoreCollectionName { get; set; }
+        string DndBackgroundCollectionName { get; set; }
+        string DndClassCollectionName { get; set; }
+        string DndEquipmentCollectionName { get; set; }
+        string DndEquipmentCategoriesCollectionName { get; set; }
+        string DndFeatsCollectionName { get; set; }
+        string DndFeaturesCollectionName { get; set; }
+    }
+}
